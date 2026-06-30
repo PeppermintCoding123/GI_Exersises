e27550f42b7fa1f4a1e64af39b3d06719774becc
Lisabeth <lisabeth.hasse@gmail.com>
2026-06-30 18:29:07 +0200

    Ex4 A2 add pathtracer with shadow-ray-addition

--- src/algorithms/pathtracer.cpp
+++ src/algorithms/pathtracer.cpp
@@ -15,16 +15,66 @@ struct Pathtracer : public Algorithm {
     inline static const std::string name = "Pathtracer";
 
     void sample_pixel(Context& context, uint32_t x, uint32_t y, uint32_t samples) {
+        vec3 accum = vec3(0.f);
         for (uint32_t i = 0; i < samples; ++i) {
-            vec3 L(0);
-
             // TODO ASSIGNMENT4
             // - implement a pathtracer using next event estimation
             // - add russian roulette
             // - (optional, bonus) add multiple importance sampling
 
-            context.fbo.add_sample(x, y, L);
+            // setup view ray
+            Ray ray = context.cam.view_ray(
+                        x, y, context.fbo.width(), context.fbo.height(), RNG::uniform<vec2>(), RNG::uniform<vec2>()
+            );
+
+            vec3 throughput = vec3(1.f);
+            vec3 L = vec3(0.f);
+
+            for (int depth = 0; depth < context.MAX_CAM_PATH_LENGTH; ++depth) {
+                SurfaceHit hit = context.scene.intersect(ray);
+
+                // Hit a light source
+                if (hit.is_light() && hit.valid) {
+                    // use AreaLight
+                    vec3 Le = hit.valid ? hit.Le() : context.scene.Le(ray);
+                    L += throughput * Le;
+                    break;
+                }
+                if (hit.is_light() && !hit.valid) {
+                    // use SkyLight
+                    vec3 Le = context.scene.Le(ray); // environment/ambient
+                    L += throughput * Le;
+                    break;
+                }
+                if (hit.valid){
+                    // TODO: sample Light
+                    const auto [light, pdf_light_source] = context.scene.sample_light_source(RNG::uniform<float>());
+                    auto [Li, shadow_ray, pdf_light_sample] = light->sample_Li(hit.P, RNG::uniform<vec2>());
+                    const float pdf_L = pdf_light_source * pdf_light_sample;
+                    if (pdf_L <= 0.f) break;
+
+                    if (pdf_L > 0.f && !context.scene.occluded(shadow_ray)){
+                        L += Li * throughput * hit.f(-ray.dir, shadow_ray.dir) * fmaxf(0.f, dot(hit.N, shadow_ray.dir)) / pdf_L;
+                    }
+
+                    const auto [brdf, w_i, pdf] = hit.sample(-ray.dir, RNG::uniform<vec2>());
+                    if (pdf <= 0.f) break;
+
+                    float cos_theta = std::max(0.f, dot(hit.N, w_i));
+                    vec3 throughput_next = throughput * brdf * cos_theta / pdf;
+
+                    ray = Ray(hit.P, w_i);
+                    throughput = throughput_next;
+                }
+            }
+
+            // Accumulate to framebuffer (ensuring non-negative)
+            accum += max(vec3(0.f), L);
         }
+
+        // Average over samples and write to framebuffer
+        vec3 final = accum / float(samples);
+        context.fbo.add_sample(x, y, final);
     }
 };
 
